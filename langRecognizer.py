464c3f00cf1e7263c98de483110c0b207f4495d1
katrinp <atrit@seznam.cz>
2014-09-18 21:51:07 +0200

    version 1.0 to use :)

--- langRecognizer.py
+++ langRecognizer.py
@@ -1,15 +1,19 @@
 import ngrams, operator, collections, langVector, sys, argparse 
 
-#HOW TO IMPROVE: use collections.OrderedDict (pro záznam skóre), ask for creating new vector file
-#TODO: shoda u určení jazyků
-#TODO: váhy! - viz HOW TO IMROVE
+#HOW TO IMPROVE: ask for creating new vector file
 #MAYBE: improve the command line arguments???
+
+#constants:
+smoothing_rate = 1.5
+ngram_rate = 1
+number_of_ngrams = 3
 
 #count a score for sentence from vector of a language ~ probability for ngrams in the language (suma of logarithms)
 def count_ngram_score(sentence, vector, n):
 	score = 0
 	ngrams_array = ngrams.make_ngrams(sentence, n)
 	#smoothing - for ngrams which don't appear in vector of language
+	#set the worst (max) value in lang vector 
 	smoothing = max(vector[n-1].items(), key=operator.itemgetter(1))[0]
 
 	for ngram in ngrams_array:	
@@ -18,65 +22,59 @@ def count_ngram_score(sentence, vector, n):
 			#print("add ", vector[n-1][ngram], "for ", ngram)
 		else:
 			#print("not found ", ngram, "add ", vector[n-1][smoothing] )
-			score += vector[n-1][smoothing]/1.5 #zde pořešit míru, 1 byla moc přísná 
+			score += vector[n-1][smoothing]/smoothing_rate 
 	return score
 
-#n = number of kinds of ngrams (uni + bi + trigram = 3)
+#n = number of kinds of used ngrams (uni + bi + trigram = 3)
 def recognize_language(sentence, vectors, n):
 	scores = []
+
+	#score is list which contents a dictionary for each ngram. 
+	#Key in dict is language, value is the for for given sentence
 	for i in range(0,n):
 		scores.append({})
 		for language in vectors.keys():
 			#print("work with ", language)
 			scores[i][language] = count_ngram_score(sentence, vectors[language], i+1)
-	print(scores)
-	#result for uni/bi/trigrams - 0 ~ uni etc.
-	result = []
-	#result2 = {}
-	result2 = collections.defaultdict(int)
 
+	#print(scores)
+	#result for uni/bi/trigrams - 0 ~ uni etc.
+	winners_for_ngram = [] #remebers which language was the best for uni, bi and trigram
+	detected_languages = collections.defaultdict(int) #key = best lang for some ngram, value is number (countet rate)
 	for i in range(0,n):
-		#find the language with largest score for i-gram
-		#result.append(min(scores[i].items(), key=operator.itemgetter(1))[0])
-		a = min(scores[i].items(), key=operator.itemgetter(1))[0] 
-		result2[a] += (i+1) * 1 #number is the weight
-		print("tomuto přičítám jedničku: ",result2[a], a)
-		result.append(a)
-		#for j in range(0,i):
-		#	result.append(a)
-	print("result2: ",result2)		
-	#counted_result = collections.Counter(result)
-	#find the languge which is mostly appeared - dořešit váhy!!!
-#	language = max(counted_result.items(), key=operator.itemgetter(1))[0]
-	highest = max(result2.values())
-	same_result = []
-	for key, value in result2.items():
+		#find the language with lowes score for i-gram
+		best_language = min(scores[i].items(), key=operator.itemgetter(1))[0] 
+		#ngram_rate tell us how much more important is the result by trigram then by unigram etc.
+		#notice and add rate if for ngram is the language the best
+		detected_languages[best_language] += (i+1) * ngram_rate
+		winners_for_ngram .append(best_language)
+
+	#find the languge which highest value
+	highest = max(detected_languages.values())
+	#find all languages with highest value 
+	identic_result = [] #if there is a draw 
+	for key, value in detected_languages.items():
 		if value == highest:
-			same_result.append(key)
-	if len(same_result) > 1:
+			identic_result.append(key)
+	#decite which language from these with identica value is the best
+	if len(identic_result) > 1:
 		out = False
-		for i in range(n, 0, - 1):
-			for language in same_result:
-				if result[i-1] == language:
-					language2 = language
-					print(language)
+		for i in range(n, 0, - 1): #choose the one acording the highest ngram (trigram result is more predicative the unigram result)
+			for language in identic_result:
+				if winners_for_ngram [i-1] == language:
+					final_language = language
 					out = True
 					break
 			if out:
 				break
 	else:
-		language2 = same_result[0]#max(result2.items(), key=operator.itemgetter(1))[0]
-	
-	probability = result2[language2] / sum(result2.values())
-#	probability = counted_result[language] / sum(counted_result.values())
-	return language2, probability
+		final_language = identic_result[0]	
+	#count probability of our result	
+	probability = detected_languages[final_language] / sum(detected_languages.values())
+	return final_language, probability 
 
-
-#parser = argparse.ArgumentParser(description='Recognize language of given sentence (text).')
-#parser.add_argument('-s', metavar='Text', dest='sentence', help='sentence (text) you want to recognize', action='store_const', const="notext58")
-#parser.add_argument('--vector_file', '-vf', metavar='File name', type=str, dest="vector_file", help='file with vetors of languages', default='language_vector.p')
-#args = parser.parse_args()   
-
+#main:
+#command line inputs and default setting od vectors file
 if len(sys.argv) > 2:
 	sentence = sys.argv[1]
 	vector_file = sys.argv[2]
@@ -87,11 +85,7 @@ else:
 	sentence = input("Your sentence: ")
 	vector_file = "language_vector.p"
 
-#if args.sentence == "notext58": 
-#	sentence = input("Your sentence: ")
-#else:
-#	sentnce = args.sentence
-
 vectors = langVector.load_vector(vector_file)
-print(recognize_language(sentence, vectors, 3))
+language, probability = recognize_language(sentence, vectors, number_of_ngrams)
+print("Given sentence is in",language, "(with", probability*100, "% probability)")
 
