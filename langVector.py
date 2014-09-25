d0b36a75277a4dabb0c52788f067c068144eef2c
katrinp <atrit@seznam.cz>
2014-09-25 11:56:48 +0200

    fixed reading from file -> process with each line (not whole file in array)

--- langVector.py
+++ langVector.py
@@ -1,14 +1,23 @@
-import ngrams, pickle, os, sys
+import ngrams, pickle, os, sys, collections
 		
 #make a propbability list (vector) for one language
 #source_file - file with plain text
 def vector_of_language(source_file):
 	opened_file = open(source_file, encoding="utf-8")
-	text = opened_file.read()
+	unigrams = [{},{}]
+	bigrams = [{},{}]
+	trigrams = [{},{}]
+	for line in opened_file:
+		unigrams[1] = ngrams.count_ngrams(line,1)
+		unigrams[0] = sum((collections.Counter(dict(lines)) for lines in unigrams), collections.Counter())
+		bigrams[1] = ngrams.count_ngrams(line,2)
+		bigrams[0] = sum((collections.Counter(dict(lines)) for lines in bigrams), collections.Counter())	
+		trigrams[1] = ngrams.count_ngrams(line,3)
+		trigrams[0] = sum((collections.Counter(dict(lines)) for lines in trigrams), collections.Counter())	
 
-	unigram_probability = ngrams.probability(ngrams.count_ngrams(text,1))
-	bigram_probability = ngrams.probability_of_bigram(ngrams.count_ngrams(text, 2))
-	trigram_probability = ngrams.probability_of_trigram(ngrams.count_ngrams(text, 3))
+	unigram_probability = ngrams.probability(unigrams[0])
+	bigram_probability = ngrams.probability_of_bigram(bigrams[0])
+	trigram_probability = ngrams.probability_of_trigram(trigrams[0])
 	return [unigram_probability, bigram_probability, trigram_probability]
 
 def add_language_vector(language, source_file, vector_file):
@@ -16,7 +25,7 @@ def add_language_vector(language, source_file, vector_file):
 	vectors[language] =  vector_of_language(source_file)
 	with open(vector_file, "wb" ) as f:
 		pickle.dump(vectors, f)
-	print(*("I have learned ",language,"!"), sep="")
+	print("I have learned "+language+"!")
 	
 	return
 
@@ -28,5 +37,5 @@ def load_vector(vector_file):
 	if vectors == {}:
 		user_answer = input("File with language vectors was empty or didn't exist. New empty set of vectors will be created. OK? (Y/n): ")
 		if user_answer.lower() == "n":
-			sys.exit("Work with no vector file is not possible, sorry!")
+			sys.exit("Work without any vector file is not possible, sorry!")
 	return vectors
